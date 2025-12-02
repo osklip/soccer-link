4960b8d05ad5b9fdfa93e0dfe55e694b2eb2265d
osklip <lipskioskar23@gmail.com>
2025-12-02 19:02:29 +0100

    Drobne zmiany

--- Services/DatabaseConfig.cs
+++ Services/DatabaseConfig.cs
@@ -13,13 +13,10 @@ namespace SoccerLink.Services
         public static async Task<IDatabaseClient> CreateClientAsync()
         {
             return await DatabaseClient.Create(o => {
-                o.Url = Url;
-                o.AuthToken = Token;
+                o.Url = Secrets.Url;
+                o.AuthToken = Secrets.Token;
                 o.UseHttps = true;
             });
         }
-
-        public const string Url = "https://soccerlinkdb-enbixd.aws-eu-west-1.turso.io";
-        public const string Token = "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJleHAiOjE3OTU2MzcwODksImdpZCI6ImNhOWI1NGU3LTMwY2QtNDA5YS04YTMzLTcyMmRmZDFiYWY0YiIsImlhdCI6MTc2NDEwMTA4OSwicmlkIjoiYTBiNTRjM2YtZmZkYy00MjIyLWI2YTEtZGRhZTcxN2I1MmY4In0.dnupQBG2k5tiShROTpDhcHjm8b36JHLd4tebvAWESVZ-PtLlz40gq0ywuhf3c9MefzIFmZLkTVCZpgm5dw20Dg";
     }
 }
