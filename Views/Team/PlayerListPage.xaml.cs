63055cec1be09bb670ff2f1d9ecec484cf3ad332
pkornacki0 <UL0284700@edu.uni.lodz.pl>
2026-01-13 22:25:45 +0100

    poprawki

--- Views/Team/PlayerListPage.xaml.cs
+++ Views/Team/PlayerListPage.xaml.cs
@@ -1,26 +1,45 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
 using SoccerLink.ViewModels.Team;
-using System;
 
 namespace SoccerLink.Views
 {
     public sealed partial class PlayerListPage : Page
     {
-        public PlayerListViewModel ViewModel { get; }
+        // Instancja ViewModelu przypisana do strony
+        public PlayerListViewModel ViewModel { get; } = new PlayerListViewModel();
 
         public PlayerListPage()
         {
-            ViewModel = new PlayerListViewModel();
             this.InitializeComponent();
-
-            ViewModel.RequestNavigateBack += (s, e) => this.Frame.Navigate(typeof(TeamManagementPage));
-            this.Loaded += PlayerListPage_Loaded;
         }
 
-        private async void PlayerListPage_Loaded(object sender, RoutedEventArgs e)
+        // £adowanie danych przy wejœciu na stronê
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
+            base.OnNavigatedTo(e);
             await ViewModel.LoadPlayersAsync();
+        }
+
+        // --- Obs³uga przycisku WSTECZ (bezpoœrednia nawigacja) ---
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Frame != null && this.Frame.CanGoBack)
+            {
+                this.Frame.GoBack();
+            }
+        }
+
+        // --- Obs³uga przycisku WYMIEÑ (z wnêtrza listy) ---
+        private async void ReplaceButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Pobieramy przycisk i element, do którego jest przypisany (DataContext)
+            if (sender is Button btn && btn.DataContext is PlayerDisplayItem item)
+            {
+                // Wywo³ujemy logikê wymiany w ViewModelu dla konkretnego zawodnika
+                await ViewModel.ReplacePlayerAsync(item.Source);
+            }
         }
     }
 }
