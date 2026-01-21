4732967b697f241597b51953003be7f287f9d586
osklip <lipskioskar23@gmail.com>
2026-01-21 13:41:03 +0100

    release

---
+++ Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerLink
+{
+    public static class Constants
+    {
+        public const string DatabaseUrl = "https://soccerlinkdb-enbixd.aws-eu-west-1.turso.io";
+
+        public const string DatabaseToken = "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJnaWQiOiJjYTliNTRlNy0zMGNkLTQwOWEtOGEzMy03MjJkZmQxYmFmNGIiLCJpYXQiOjE3NjUyNzg0NjQsInJpZCI6ImEwYjU0YzNmLWZmZGMtNDIyMi1iNmExLWRkYWU3MTdiNTJmOCJ9.m7-Qj3ltdSqebDrDH4Q_c96gAbb6Bqs9xknYfDsodpwEkEslNRGAQ6N10XemtTxRvF9p4S1fzShGjWBRGvcCCQ";
+    }
+}
--- Services/DatabaseConfig.cs
+++ Services/DatabaseConfig.cs
@@ -1,4 +1,6 @@
 ﻿using Libsql.Client;
+using Microsoft.VisualBasic;
+using SoccerLink;
 using SoccerLink.Views;
 using System;
 using System.Collections.Generic;
@@ -13,17 +15,16 @@ namespace SoccerLink.Services
         public static async Task<IDatabaseClient> CreateClientAsync()
         {
             return await DatabaseClient.Create(o => {
-                
-                var url = Secrets.Url;
 
-                
+                var url = Constants.DatabaseUrl;
+
                 if (!string.IsNullOrEmpty(url) && url.StartsWith("libsql://"))
                 {
                     url = "https://" + url.Substring("libsql://".Length);
                 }
 
                 o.Url = url;
-                o.AuthToken = Secrets.Token;
+                o.AuthToken = Constants.DatabaseToken;
                 o.UseHttps = true;
             });
         }
